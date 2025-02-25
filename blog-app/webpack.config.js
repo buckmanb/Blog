6c744cb932ba428da66de2eaf6f6908f927911dc
buckmanb <bbuckman@gmail.com>
2025-02-25 03:06:29 -0600

    Try to include environment variables when deploying in github

--- blog-app/webpack.config.js
+++ blog-app/webpack.config.js
@@ -1,25 +1,9 @@
-// const Dotenv = require('dotenv-webpack');
-
-// const webpack = require('webpack');
-// const { merge } = require('webpack-merge');
-// const AngularWebpackPlugin = require('@ngtools/webpack').AngularWebpackPlugin;
-// const path = require('path');
-const Dotenv = require('dotenv-webpack')
+const Dotenv = require('dotenv-webpack');
    
 module.exports = {
-  plugins: [
-    new Dotenv()
-  ]
-}
-   
-// module.exports = (config) => {
-//   return merge(config, {
-//     plugins: [
-//       new webpack.DefinePlugin({
-//         'process.env': JSON.stringify({
-//           SOME_ENV_VARIABLE: 'your_value_here'
-//         })
-//       })
-//     ]
-//   });
-// };
+    plugins: [
+      new Dotenv({
+        systemvars: true, //Set to true if you would rather load all system variables as well (useful for CI purposes)
+      })
+    ]    
+  };   
